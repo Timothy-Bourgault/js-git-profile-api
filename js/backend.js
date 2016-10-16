11c09c074d012afbd4c90956639ed32f424ca27e
Tim <tim.bourgault@gmail.com>
2016-10-16 15:38:52 -0700

    set up api key in .env file and link it to .js files.

---
+++ js/backend.js
@@ -0,0 +1,7 @@
+var apiKey = require('./../.env').apiKey;
+
+function GitUser() {
+
+}
+
+exports.gitUserModule = GitUser;
---
+++ js/interface.js
@@ -0,0 +1,5 @@
+var GitUser = require('./../js/backend.js').gitUserModule;
+
+$(document).ready(function) {
+  
+}
