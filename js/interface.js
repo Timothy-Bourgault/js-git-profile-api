9de0c5461e27a7611473937b4a2699385df18a45
Tim <tim.bourgault@gmail.com>
2016-10-16 15:56:59 -0700

    update user interface with a document ready function.

--- js/interface.js
+++ js/interface.js
@@ -1,5 +1,10 @@
 var GitUser = require('./../js/backend.js').gitUserModule;
 
 $(document).ready(function) {
-  
+  var newGitUser = new GitUser();
+  $('#inputForm').submit(function(event) {
+    event.preventDefault();
+    var userName = $('#userName').val();
+
+  })
 }
