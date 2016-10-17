dc1d79b67ae5176057ab8e39a558b00db79d56e1
Tim <tim.bourgault@gmail.com>
2016-10-17 07:58:01 -0700

    add display user function.

--- js/backend.js
+++ js/backend.js
@@ -1,16 +1,17 @@
-// var apiKey = require('./../.env').apiKey;
+var apiKey = require('./../.env').apiKey;
 
 function GitUser(){
 }
 
-GitUser.prototype.getUser = function(userName) {
-  $.get('https://api.github.com/users/' + userName + '?access_token=' + "deb5038e55957593032b746c927587b66520971f").then(function(response) {
+GitUser.prototype.getUser = function(username, displayUser) {
+  $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey ).then(function(response) {
     console.log(response);
     // console.log(response.name);
-    return response;
+    displayUser(response);
   }).fail(function(error) {
-    $('#showUser').text(error.responseJSON.message);
+    errorMessageFunction();
+    $('#errorMessage').text(error.responseJSON.message);
   });
 };
 
-exports.gitUserModule = GitUser;
+exports.getGitUser = GitUser;
--- js/user-interface.js
+++ js/user-interface.js
@@ -1,8 +1,16 @@
-var GitUser = require('./../js/backend.js').gitUserModule;
+var GitUser = require('./../js/backend.js').getGitUser;
+
+var displayUser = function(response) {
+  $('#showUser').text('');
+  $('#showUser').append("<h2>" + response.name + "</h2>");
+  $('#showUser').append("<img src='" + response.avatar_url +"' class='img-responsive img-thumbnail' asl='GitHub Avatar'>");
+  $('#showUser').append("<p>Joined GitHub " + moment(response.created_at).format('LLL') + "</p>");
+  $('#showUser').append("<h3>" + response.email + "</h3>");
+};
 
 $(document).ready(function() {
   var currentGitUser = new GitUser();
-  $('#inputForm').submit(function(event) {
+  $('#searchBtn').click(function(event) {
     event.preventDefault();
     var userName = $('#userName').val();
     $('#userName').val('');
